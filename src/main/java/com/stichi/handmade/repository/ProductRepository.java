3677049e4d11290866cf814c453709bf5389268d
Roxana Carabas <carabasroxana@yahoo.com>
2025-05-31 16:29:10 +0300

    adding Spring Data JPA to ProductRepository

---
+++ src/main/java/com/stichi/handmade/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.stichi.handmade.repository;
+
+import com.stichi.handmade.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
