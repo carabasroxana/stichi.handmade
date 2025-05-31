7db5fc773df719d0ce6ad20edff8c5dbc57afb7c
Roxana Carabas <carabasroxana@yahoo.com>
2025-05-31 16:29:39 +0300

    adding ProductService

---
+++ src/main/java/com/stichi/handmade/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.stichi.handmade.service;
+
+import com.stichi.handmade.model.Product;
+import com.stichi.handmade.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+}
