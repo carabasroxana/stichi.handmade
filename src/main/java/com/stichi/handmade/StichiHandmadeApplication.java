c3542cc7d3a0a870cdcbc646cdae6e1993dcaa87
Roxana Carabas <carabasroxana@yahoo.com>
2025-05-31 16:27:22 +0300

    application main file

---
+++ src/main/java/com/stichi/handmade/StichiHandmadeApplication.java
@@ -0,0 +1,11 @@
+package com.stichi.handmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StichiHandmadeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StichiHandmadeApplication.class, args);
+    }
+}
