5e1de4a79028cd8ef4a78bdfca5abbaf5bb30e60
Roxana Carabas <carabasroxana@yahoo.com>
2025-06-02 23:54:04 +0300

    add data.sql to seed initial product entries

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO PRODUCT(name, description, stock_count, in_stock, lead_time_days, available_colors, custom_options, price)
+VALUES('Crochet Beanie', 'Warm crocheted beanie for winter.', 5, TRUE, NULL, 'red,blue,green', 'size', 30.0);
+
+INSERT INTO PRODUCT(name, description, stock_count, in_stock, lead_time_days, available_colors, custom_options, price)
+VALUES('Handmade Scarf', 'Long scarf with intricate patterns.', 0, FALSE, 7, 'white,cream,gray', 'length,color', 45.0);
+
+INSERT INTO PRODUCT(name, description, stock_count, in_stock, lead_time_days, available_colors, custom_options, price)
+VALUES('Crochet Sweater', 'Soft sweater, customizable.', 0, FALSE, 14, 'pink,black,navy', 'size,color,pattern', 120.0);
