14a5448c31d9018bde373fe2fa78b2990e6f5ad3
Roxana Carabas <carabasroxana@yahoo.com>
2025-07-11 20:36:51 +0300

    Fix AOS links to HTTPS and apply Home page styling changes

--- src/main/java/com/stichi/handmade/config/WebConfig.java
+++ src/main/java/com/stichi/handmade/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.stichi.handmade.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -33,4 +34,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
+    }
+
 }
